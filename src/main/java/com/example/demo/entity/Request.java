756bf791c17ed739bf33d0aa73082ba379a99c29
anettaj <58222128+anettaj@users.noreply.github.com>
2020-12-20 02:07:38 +0530

    Update Request.java

--- src/main/java/com/example/demo/entity/Request.java
+++ src/main/java/com/example/demo/entity/Request.java
@@ -5,8 +5,7 @@ import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -17,7 +16,7 @@ import org.hibernate.annotations.OnDeleteAction;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sun.istack.NotNull;
+
 
 @Entity
 @Table(name = "REQUEST")
